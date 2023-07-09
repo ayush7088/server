310c21498536ffacd84cc19ae8c7675491d21756
ayush7088 <ayushjoshi282@gmail.com>
2023-07-10 03:23:16 +0530

    feat: added more server functionalities.

--- Routes/PostRoute.js
+++ Routes/PostRoute.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { createPost, getPost } from '../Controllers/PostController.js';
+import { createPost, deletePost, getPost, getTimelinePosts, likePost, updatePost } from '../Controllers/PostController.js';
 const router = express.Router();
 
 // Create new post
 router.post('/', createPost);
-router.get('/:id',getPost)
+router.get('/:id', getPost)
+router.put('/:id', updatePost)
+router.delete('/:id',deletePost)
+router.put("/:id/like", likePost)
+router.get("/:id/timeline",getTimelinePosts)
 export default router;
